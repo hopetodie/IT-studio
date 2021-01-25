ba14cef76deb8fb29717ca8b202cbff209cba8f5
Ali <hopetodie1@gmail.com>
2021-01-25 22:58:02 +0600

    It-studio-adaptive

---
+++ src/js/jeemboE.js
@@ -0,0 +1,4 @@
+document.querySelector('.burger').addEventListener('click', function ()  {
+document.querySecelctor('.burger').classList.toggle('active')
+document.querySecelctor('.nav').classList.toggle('active')
+})
